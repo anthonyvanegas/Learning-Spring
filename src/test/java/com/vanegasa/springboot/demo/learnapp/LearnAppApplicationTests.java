addd110fe7f436d863e1a6c47019c524b117b20b
Anthony Vanegas <antvanegas@icloud.com>
2026-02-14 14:27:58 -0500

    first commit

---
+++ src/test/java/com/vanegasa/springboot/demo/learnapp/LearnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanegasa.springboot.demo.learnapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
